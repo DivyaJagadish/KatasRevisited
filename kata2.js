f0607cca2472aaaa3632037459331e246171b44d
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-05 02:54:44 +0000

    commit after refactoring

---
+++ kata2.js
@@ -0,0 +1,13 @@
+const conditionalSum = function(values, condition) {
+  let evensum = 0, oddsum = 0;
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] % 2 === 0) evensum += values[i];
+    else oddsum += values[i];
+  } if (condition === "even") return evensum;
+  else return oddsum;
+};
+  
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
---
+++ kata8.js
@@ -0,0 +1,14 @@
+const repeatNumbers = function(data) {
+  let result = "";
+  for (let i = 0; i < data.length; i++) {
+    for (let j = data[i][1]; j > 0; j--)
+      result += data[i][0];
+    if (i !== data.length - 1)
+      result += ", ";
+  }
+  return result;
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
